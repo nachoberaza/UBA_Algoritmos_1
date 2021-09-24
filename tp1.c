4c017bf21c0a444a1e78c020a4be636c4879dce8
Matias Perez <85464454+matias205@users.noreply.github.com>
2021-09-24 08:03:19 -0300

    ahi anote las cosas, vi que hay un tema con cuenta

--- tp1.c
+++ tp1.c
@@ -1,69 +1,60 @@
-//r(t)= O+td  recta
-//C centro, R radio, 
-// T para interseccion
-//si es+ hay interseccion
 #include <stdio.h>
 #include <math.h>
 
-#define CX  0.5
-#define CY  0.75
-#define CZ  1.9
-#define R   0.8
+#define CX 0.5
+#define CY 0.75
+#define CZ 1.9
+#define R 0.8
 #define ANCHO 640
 #define ALTO 480
 #define FOV 90
 
-float norma(float x, float y, float z){
-    int xalcuadrado= pow(x,2);
-    int yalcuadrado= pow(y,2);
-    int zalcuadrado= pow(z,2);
-    float normaR = sqrt(xalcuadrado+yalcuadrado+zalcuadrado);
+float norma(float x, float y, float z)
+{
+    int xalcuadrado = pow(x, 2);
+    int yalcuadrado = pow(y, 2);
+    int zalcuadrado = pow(z, 2);
+    float normaR = sqrt(xalcuadrado + yalcuadrado + zalcuadrado);
     return normaR;
 }
-float producto_interno(float x1, float y1, float z1, float x2, float y2, float z2){
-    float producto_internoR = (x1*x2)+(y1*y2)+(z1*z2);
-	return producto_internoR;
-}
-float producto_interno1(float CX, float CY,float CZ,int d[3]){  //el puntero estaria con lo comentado en el main
-	float producto_interno1R = (CX*d[0])+(CY*d[1])+(CZ*d[2]);
-    return producto_interno1R;
-}
-float producto_interno2(float CX, float CY,float CZ){
-	float producto_interno2R= (CX*CX)+(CY*CY)+(CZ*CZ);
-    return producto_interno2R ;
-}
 
-float T = pow(producto_interno1,2) - (producto_interno2) + pow(R,2);
+float CC;			  //variable para producto interno C*C
 
-int intersecta_esfera(float dx, float dy, float dz){
-    //float T = pow(producto_interno,2) - (producto_interno2) + pow(R,2);
-    if(T >0)
-        printf("1");
-    else{
-        if(T=0)
-            printf("0");
-    }
+float CD; 			  //variable para producto interno C*D
+
+
+float  Z = ANCHO / 2; //la tangente de (90°/2) es = a 1
+
+float X= -ANCHO / 2; //coordenada de donde empiezan las X
+
+float Y= -ALTO / 2; //coordenada donde empiezan las Y
+
+float producto_interno( float X,  float Y, float Z)
+{
+    CD = (CX * X) + (CY * Y) + (CZ * Z);
+    CC = (CX * CX) + (CY * CY) + (CZ * CZ);
     return 0;
 }
-int main(){
-	/*float norma (int &d[i]){       con los valores ingresados del vector d saco la norma
-		int xalcuadrado= pow(*d[0],2);
-    	int yalcuadrado= pow(*d[1],2);
-    	int zalcuadrado= pow(*d[2],2);
-    	float normaR = sqrt(xalcuadrado+yalcuadrado+zalcuadrado);
-    return normaR;
-		
-	}*/                             
-    int *d[3];// seria el ingreso del vector d
-    for(int i=0;i<=3;i++){ 
-    printf("ingrese valores de d");
-    scanf(&d[i]);
-    }
 
-    scanf()
-    for(int w=0; w>=27; w++){  //intento de matriz para recorrer los pixeles
-    for(int h;h>=7;h++){
-        printf("d%",intersecta_esfera); //que pngo va%%
+int main(void)
+{
+    printf("p1 \n");
+    printf("640x480 \n");
+
+    for (X; X <= 640; X++) 							//recorro las X desde -ancho/2 hasta el final
+    { 
+        for (Y; Y <= 480; Y++)  					//recorro las y desde -alto/2 hasta el final
+        {
+            float T = pow(CD, 2) - CC + pow(R, 2);  //interseccion
+            //temita con la cuentaaa****************dan todos positivos, lo comprove con print cd y cc
+            if (T >= 0)								//comprovar si existe interseccion
+            {
+                printf("1"); 						//1 si interseca
+            }
+            else
+            {
+                printf("0");  						//0 si no interseca
+            }
+        }
     }
-}
 }
